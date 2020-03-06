4ec9cb16e8d7f62bfa27722dfe25deaf181e2d4a
Quan Chau <quan.chau@appfolio.com>
2020-03-06 15:20:04 -0800

    test helper method to temporarily enable the asset pipeline

--- lib/ae_skip_asset_pipeline.rb
+++ lib/ae_skip_asset_pipeline.rb
@@ -6,28 +6,47 @@ require 'ae_skip_asset_pipeline/version'
 module AeSkipAssetPipeline
   mattr_accessor :enabled
 
-  def javascript_packs_with_chunks_tag(*args)
-    super unless AeSkipAssetPipeline.enabled
+  module AssetPipelineMethods
+    def javascript_packs_with_chunks_tag(*args)
+      super unless AeSkipAssetPipeline.enabled
+    end
+
+    def javascript_include_tag(*args)
+      super unless AeSkipAssetPipeline.enabled
+    end
+
+    def stylesheet_packs_with_chunks_tag(*args)
+      super unless AeSkipAssetPipeline.enabled
+    end
+
+    def stylesheet_link_tag(*args)
+      super unless AeSkipAssetPipeline.enabled
+    end
+
+    def path_to_asset(source, options={})
+      super(source, options.merge(skip_pipeline: AeSkipAssetPipeline.enabled))
+    end
   end
 
-  def javascript_include_tag(*args)
-    super unless AeSkipAssetPipeline.enabled
-  end
+  module HelperMethods
+    def save_current_state_and_set_asset_pipeline_enabled(enabled = true)
+      @current_skip_status = AeSkipAssetPipeline.enabled
+      AeSkipAssetPipeline.enabled = !enabled
+    end
 
-  def stylesheet_packs_with_chunks_tag(*args)
-    super unless AeSkipAssetPipeline.enabled
-  end
+    def restore_old_state_of_asset_pipeline_enabled
+      AeSkipAssetPipeline.enabled = @current_skip_status
+    end
 
-  def stylesheet_link_tag(*args)
-    super unless AeSkipAssetPipeline.enabled
-  end
-
-  def path_to_asset(source, options = {})
-    super(source, options.merge(skip_pipeline: AeSkipAssetPipeline.enabled))
+    def with_asset_pipeline_enabled(enabled = true)
+      save_current_state_and_set_asset_pipeline_enabled(enabled)
+      yield
+      restore_old_state_of_asset_pipeline_enabled
+    end
   end
 end
 
 AeSkipAssetPipeline.enabled = true
 
-ActionView::Base.prepend AeSkipAssetPipeline
+ActionView::Base.prepend AeSkipAssetPipeline::AssetPipelineMethods
 
